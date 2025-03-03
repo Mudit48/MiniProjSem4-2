655e00c083d4b0c9bc9386a8da8fc003d84a89af
Mudit48 <muditmeshram48@gmail.com>
2025-03-03 12:15:41 +0530

    list UI

--- ActivityNest/main/forms.py
+++ ActivityNest/main/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 from .models import Item
+from .models import category_items, year_choice
 
 
 class ListForm(forms.ModelForm):
-    department = forms.CharField(widget=forms.TextInput(attrs={'readonly' : 'readonly'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={ 'class' : 'text-black ring-black ring-1 rounded-lg h-5'}))
+    department = forms.CharField(widget=forms.TextInput(attrs={'readonly' : 'readonly', 'class' : 'text-black '}))
+    category = forms.ChoiceField(choices=category_items, widget=forms.Select(attrs={'class' : 'text-black'}))
+    year = forms.ChoiceField(choices=year_choice, widget=forms.Select(attrs={'class' : 'text-black'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={'class' : 'text-black ring-black ring-1 rounded-lg h-5'}))
+
+
 
     class Meta:
         model = Item
